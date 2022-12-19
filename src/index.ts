a161959806e40953891ffbae1288418c93566c82
kimminwook <coreminw@naver.com>
2022-12-19 22:38:49 +0900

    [FEAT] cors 에러 처리

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express, { NextFunction, Request, Response } from "express";
 import router from "./router";
-
 const app = express(); // express 객체 받아옴
 const PORT = 3000; // 사용할 port를 3000번으로 설정
+const cors = require("cors");
 
+
+let corsOptions = {
+  origin: "*",
+  credential: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json()); // express 에서 request body를 json 으로 받아오겠다.
 
 app.use("/", router); // use -> 모든 요청
